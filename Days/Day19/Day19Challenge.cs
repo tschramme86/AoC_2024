2d620e200ac3a738b68ce1e76e659cc5c6595c35
Torben Schramme <t.schramme@live.de>
2024-12-20 16:13:23 +0100

    Day 19

---
+++ Days/Day19/Day19Challenge.cs
@@ -0,0 +1,80 @@
+﻿using Google.OrTools.LinearSolver;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AoC2024.Days.Day19
+{
+    internal partial class Day19Challenge : AoCChallengeBase
+    {
+        public override int Day => 19;
+        public override string Name => "Linen Layout";
+
+        protected override object ExpectedTestResultPartOne => 6L;
+        protected override object ExpectedTestResultPartTwo => 16L;
+
+        private readonly List<string> _availableTowels = [];
+        private readonly List<string> _desiredPatterns = [];
+
+        protected override object SolvePartOneInternal(string[] inputData)
+        {
+            this.ParseTowels(inputData);
+            var result = 0L;
+            foreach(var p in this._desiredPatterns)
+            {
+                result += (this.CalcPossibleTowelArrangements(p) > 0 ? 1 : 0);
+            }
+            return result;
+        }
+
+        protected override object SolvePartTwoInternal(string[] inputData)
+        {
+            this.ParseTowels(inputData);
+            var result = 0L;
+            foreach (var p in this._desiredPatterns)
+            {
+                result += this.CalcPossibleTowelArrangements(p);
+            }
+            return result;
+        }
+
+        private long CalcPossibleTowelArrangements(string desiredPattern)
+        {
+            Func<int, long> fnArrangementFinder = _ => throw new NotImplementedException();
+            fnArrangementFinder = FnCache.Make((int index) =>
+            {
+                if (index >= desiredPattern.Length)
+                {
+                    return 1L;
+                }
+                long possibleArrangements = 0;
+                foreach (var towel in this._availableTowels)
+                {
+                    if (desiredPattern.AsSpan()[index..].StartsWith(towel))
+                    {
+                        var tails = fnArrangementFinder(index + towel.Length);
+                        possibleArrangements += tails;
+                    }
+                }
+                return possibleArrangements;
+            });
+            return fnArrangementFinder(0);
+        }
+
+        private void ParseTowels(string[] inputData)
+        {
+            this._availableTowels.Clear();
+            this._desiredPatterns.Clear();
+
+            this._availableTowels.AddRange(inputData[0].Split(',').Select(x => x.Trim()));
+            foreach(var p in inputData.Skip(2))
+            {
+                this._desiredPatterns.Add(p);
+            }
+        }
+    }
+}
---
+++ FnCache.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC2024
+{
+    public static class FnCache
+    {
+        public static Func<int, TR> MakeInt<TR>(Func<int, TR> fn)
+            where TR : class
+        {
+            List<TR?> memo = new();
+            return x1 =>
+            {
+                if (x1 < 0)
+                {
+                    throw new ArgumentException("Argument must be zero or greater");
+                }
+                lock (memo)
+                {
+                    if (memo.Count <= x1)
+                    {
+                        memo.AddRange(Enumerable.Repeat(default(TR?), x1 - memo.Count + 1));
+                    }
+                }
+                return memo[x1] ?? (memo[x1] = fn(x1));
+            };
+        }
+
+        public static Func<T1, TR> Make<T1, TR>(Func<T1, TR> fn)
+            where T1 : notnull
+        {
+            ConcurrentDictionary<T1, TR> memo = new();
+            return x1 =>
+            {
+                if (memo.TryGetValue(x1, out var result))
+                {
+                    return result;
+                }
+                return memo[x1] = fn(x1);
+            };
+        }
+        public static Func<T1, T2, TR> Make<T1, T2, TR>(Func<T1, T2, TR> fn)
+        {
+            ConcurrentDictionary<(T1, T2), TR> memo = new();
+            return (x1, x2) =>
+            {
+                if (memo.TryGetValue((x1, x2), out var result))
+                {
+                    return result;
+                }
+                return memo[(x1, x2)] = fn(x1, x2);
+            };
+        }
+        public static Func<T1, T2, T3, TR> Make<T1, T2, T3, TR>(Func<T1, T2, T3, TR> fn)
+        {
+            ConcurrentDictionary<(T1, T2, T3), TR> memo = new();
+            return (x1, x2, x3) =>
+            {
+                if (memo.TryGetValue((x1, x2, x3), out var result))
+                {
+                    return result;
+                }
+                return memo[(x1, x2, x3)] = fn(x1, x2, x3);
+            };
+        }
+    }
+}
